c4c417412568209e8e3c37ea86ec96ceaf148eb3
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-06 15:41:28 +0900

    名前に関するvalidationを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nick_name, presence: true
+
+  kanji_reg = '\A[一-龥]+\z'
+  validates :name_last_w, :name_first_w, presence: true, format: { with: /#{kanji_reg}/,
+                                                                   message: 'is not Kanji' }
+  katakana_reg = '\A[ァ-ヶー－]+\z'
+  validates :name_last_r, :name_first_r, presence: true, format: { with: /#{katakana_reg}/,
+                                                                   message: 'is not full width Katakana' }
 end
