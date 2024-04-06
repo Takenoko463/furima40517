eb123abfbbc7fe8df72a0b58214283ea0b5b4a37
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-06 19:27:13 +0900

    生年月日のvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nick_name, presence: true
 
+  ## 本名に関する規制
   kanji_reg = '\A[一-龥]+\z'
   validates :name_last_w, :name_first_w, presence: true, format: { with: /#{kanji_reg}/,
                                                                    message: 'is not Kanji' }
   katakana_reg = '\A[ァ-ヶー－]+\z'
   validates :name_last_r, :name_first_r, presence: true, format: { with: /#{katakana_reg}/,
                                                                    message: 'is not full width Katakana' }
+  ## 誕生日の最小、最大値
+  start_date = Date.new(1920, 1, 1)
+  ## Date.today実行時にvalidationを避ける
+  end_date = ->(_x) { Date.today }
+  validates :birth_day, comparison: { greater_than: start_date, less_than: end_date[0] }
 end
