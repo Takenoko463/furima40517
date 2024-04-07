27a181eae7bd80c50377feb41d9027988767965c
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 14:04:23 +0900

    配送元の地域をテーブルに追加

--- db/migrate/20240407031352_create_items.rb
+++ db/migrate/20240407031352_create_items.rb
@@ -6,6 +6,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.integer :category_id, null: false
       t.integer :condition_id, null: false
       t.integer :which_delivery_payment_id, null: false
+      t.integer :prefecture_id, null:false
       t.integer :time_for_delivery_id, null: false
       t.integer :price, null: false
       t.references :user ,null:false,foreign_key: true
