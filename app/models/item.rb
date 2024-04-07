baf913c503914aae16288ef44099d817b2642ff2
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 12:40:24 +0900

    テーブル間のアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :name, length: { maximum: 40 }
+  validates :description, length: { maximum: 1000 }
+  validates :category_id, comparison: { less_than: 4 }
+  validates :condition_id, comparison: { less_than: 7 }
+  validates :which_delivery_payment_id, comparison: { less_than: 3 }
+  validates :prefecture_id, comparison: { less_than: 48 }
+  validates :time_for_delivery_id, comparison: { less_than: 4 }
+  validates :price, comparison: { greater_than: 299, less_than: 10_000_000 }
+
+  belongs_to :user
 end
