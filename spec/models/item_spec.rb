63d8abdcfa12f6de2a120f6bc026d8af4a296fd2
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 21:25:30 +0900

    アクションハッシュidに関するtestを改良 空白を1に

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,27 +27,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Description can't be blank"
       end
       it 'category_idが空では登録できない' do
-        @item.category_id = ''
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
       it 'condition_idが空では登録できない' do
-        @item.condition_id = ''
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Condition can't be blank"
       end
       it 'which_delivery_payment_idが空では登録できない' do
-        @item.which_delivery_payment_id = ''
+        @item.which_delivery_payment_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Which delivery payment can't be blank"
       end
       it 'prefecture_idが空では登録できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture can't be blank"
       end
       it 'time_for_delivery_idが空では登録できない' do
-        @item.time_for_delivery_id = ''
+        @item.time_for_delivery_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Time for delivery can't be blank"
       end
