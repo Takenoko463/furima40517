887b154cf2c22901dd44c70b2395fb2885d9518c
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 14:18:46 +0900

    空の投稿テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,22 +8,42 @@ RSpec.describe Item, type: :model do
     it 'nameが空では登録できない' do
       @item.name = ''
       @item.valid?
-      binding.pry
       expect(@item.errors.full_messages).to include "Name can't be blank"
     end
     it 'descriptionが空では登録できない' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Description can't be blank"
     end
     it 'category_idが空では登録できない' do
+      @item.category_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Category can't be blank"
     end
     it 'condition_idが空では登録できない' do
+      @item.condition_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Condition can't be blank"
     end
     it 'which_delivery_payment_idが空では登録できない' do
+      @item.which_delivery_payment_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Which delivery payment can't be blank"
     end
     it 'prefecture_idが空では登録できない' do
+      @item.prefecture_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Prefecture can't be blank"
     end
     it 'time_for_delivery_idが空では登録できない' do
+      @item.time_for_delivery_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Time for delivery can't be blank"
     end
     it 'priceが空では登録できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Price can't be blank"
     end
   end
 end
