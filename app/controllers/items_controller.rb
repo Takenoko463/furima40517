e17d11db0f261dd61f16d90aa5cf971731ac3e4d
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 18:39:52 +0900

    サインイン状態によって、画面遷移を分類

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
     @items = Item.all
   end
@@ -27,5 +28,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:content).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
 end
