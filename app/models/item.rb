cb915e31925540656cbe251e44d06cc8858c5108
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 13:09:00 +0900

    支払い者に関する選択肢

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :description, length: { maximum: 1000 }
   validates :category_id, comparison: { less_than: 11 }
   validates :condition_id, comparison: { less_than: 8 }
-  validates :which_delivery_payment_id, comparison: { less_than: 3 }
+  validates :which_delivery_payment_id, comparison: { less_than: 4 }
   validates :prefecture_id, comparison: { less_than: 48 }
   validates :time_for_delivery_id, comparison: { less_than: 4 }
   validates :price, comparison: { greater_than: 299, less_than: 10_000_000 }
@@ -14,5 +14,5 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, numericality: { other_than: 1, message: "can't be blank" }
 
   belongs_to :user
-  belongs_to :category, :condition
+  belongs_to :category, :condition, :which_delivery_payment
 end
