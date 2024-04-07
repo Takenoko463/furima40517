4737d81c8ac5fc524db822559abfe9d9dacdfc18
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 13:33:56 +0900

    itemモデルに関してfactory bot定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Name.name }
+    description { Faker::String.random(length: [1, 1000]) }
+    category_id { Faker::Number.within(range: 2..10) }
+    condition_id { Faker::Number.within(range: 2..7) }
+    which_delivery_payment_id { Faker::Number.within(range: 2..3) }
+    prefecture_id { Faker::Number.within(range: 2..48) }
+    time_for_delivery_id { Faker::Number.within(range: 2..4) }
+    price { Faker::Number.within(range: 300..10_000_000) }
   end
 end
