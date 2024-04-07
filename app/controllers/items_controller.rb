29c365fd5db4e5ba377ed004093eba84f6d28929
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-07 17:49:53 +0900

    商品投稿画面の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
   def index
-    @items = item.all
+    @items = Item.all
+  end
+
+  def new
+    # 空の商品情報
+    @item = Item.new
+    # アクティブハッシュの情報
+    @categories = Category.all
+    @conditions = Condition.all
+    @which_delivery_payments = WhichDeliveryPayment.all
+    @prefectures = Prefecture.all
+    @time_for_deliveries = TimeForDelivery.all
   end
 end
