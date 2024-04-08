6073321e2abe9743a132d0472a0130c683a915c9
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-09 08:47:14 +0900

    商品送信先住所に関するテーブルを作成

---
+++ db/migrate/20240408234128_create_shipping_addresses.rb
@@ -0,0 +1,14 @@
+class CreateShippingAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shipping_addresses do |t|
+      t.string :postal_code, null:false
+      t.integer :prefecture_id, null:false
+      t.string :city, null:false
+      t.string :house_num, null:false
+      t.string :building_name, null:false
+      t.integer :phone_number, null:false
+      t.references :order, null:false,foreign_key:true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_08_232739) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_08_234128) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,6 +63,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_08_232739) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
+  create_table "shipping_addresses", charset: "utf8mb4", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "house_num", null: false
+    t.string "building_name", null: false
+    t.integer "phone_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_shipping_addresses_on_order_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "nick_name", default: "", null: false
     t.string "email", default: "", null: false
@@ -86,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_08_232739) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "shipping_addresses", "orders"
 end
