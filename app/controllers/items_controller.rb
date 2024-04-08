1d63d6646bfab6014ca3f5db436d6d5dffb15a84
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-08 14:56:54 +0900

    showアクション実行前に、sign_inでの振り分けを行わない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :create]
+  before_action :move_to_index, except: [:index, :create, :show]
   def index
     @items = Item.all
   end
