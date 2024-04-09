b6185768641ef0dbd2546d9d8654ec48057fb67c
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-09 14:32:17 +0900

    orderコントローラーを動かすのに、itemコントローローラーを必要とする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :orders ,only:[:index,:create,:new]
+  resources :items do
+    resources :orders ,only:[:index,:create,:new]
+  end
 end
