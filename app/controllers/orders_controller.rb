f3bbb1cf2e4f0ce6f813217baf4f1d6245f49a22
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-09 14:42:52 +0900

    アクティブハッシュと、orderを成立させるために必要な住所modelのkんスタンス変数作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
   def index
-    @item = Item.find(params[:item].id)
+    @item = Item.find(params[:item_id])
     @order = Order.new
+    @shipping_address = ShippingAddress.new
+    @prefectures = Prefecture.all
   end
 end
