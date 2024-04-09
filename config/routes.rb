dd981023c362811f792520dcf70d2cca15156758
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-09 14:07:30 +0900

    indexとcreateも許可

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-  resources :orders ,only:[:new]
+  resources :orders ,only:[:index,:create,:new]
 end
