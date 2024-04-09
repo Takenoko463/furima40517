130eddb920e03cc988c2fba6a203c0b7f58e8948
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-09 18:48:49 +0900

    元のモデルに書かれたvalidationをフォームクラスに移す

--- app/models/order_shipping_address_form.rb
+++ app/models/order_shipping_address_form.rb
@@ -3,6 +3,23 @@ class OrderShippingAddressForm
   attr_accessor :postal_code, :prefecture_id, :city, :house_num, :building_name,
                 :phone_number, :item_id, :user_id
 
+  # 空白での投稿を禁止
+  validates :postal_code, :city, :house_num, :phone_number, presence: true
+
+  # 空白である選択を禁止
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  # 電話番号、郵便番号の形式指定
+  # 　「-」必須
+  validates :postal_code, format: {
+    with: /\A\d{3}-\d{4}\z/,
+    message: 'is invalid'
+  }
+
+  validates :phone_number, format: {
+    with: /\A\d{10}\z/,
+    message: 'is invalid'
+  }
   def save
     order = Order.create(user_id:, item_id:)
     ShippingAddress.create(postal_code:, prefecture_id:, city:, house_num:, building_name:, phone_number:, order_id: order.id)
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,22 +1,5 @@
 class ShippingAddress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # 空白での投稿を禁止
-  validates :postal_code, :city, :house_num, :phone_number, presence: true
-
-  # 空白である選択を禁止
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-
-  # 電話番号、郵便番号の形式指定
-  # 　「-」必須
-  validates :postal_code, format: {
-    with: /\A\d{3}-\d{4}\z/,
-    message: 'is invalid'
-  }
-
-  validates :phone_number, format: {
-    with: /\A\d{10}\z/,
-    message: 'is invalid'
-  }
 
   # アソシエーション
   belongs_to :order
