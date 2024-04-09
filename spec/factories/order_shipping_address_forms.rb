7633a3beccb591c88c83b5a050cdc0bbaa1ead9e
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-09 21:34:03 +0900

    誤字脱字の修正、モデルではないからassociation定義不可

--- spec/factories/order_shipping_address_forms.rb
+++ spec/factories/order_shipping_address_forms.rb
@@ -1,13 +1,10 @@
 FactoryBot.define do
   factory :order_shipping_address_form do
     postal_code { '123-4567' }
-    prefecture { 5 }
+    prefecture_id { 5 }
     city { '東京都' }
-    house_number { '1-1' }
+    house_num { '1-1' }
     building_name { '東京ハイツ' }
-    phone_number { '08027328450' }
-
-    association :user
-    association :item
+    phone_number { '0992511923' }
   end
 end
