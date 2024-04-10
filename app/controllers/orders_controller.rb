6cf0d6441cdb6158ffe4d6603c61d94e9d477026
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-10 18:10:14 +0900

    orderコントローラーないでkey及び、クレジット情報を取り扱う

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -45,5 +45,10 @@ class OrdersController < ApplicationController
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
