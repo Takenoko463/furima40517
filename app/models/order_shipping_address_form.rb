94685e0b0544626d21134899bf033ba3a96d1491
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-10 18:08:56 +0900

    tokenへのvalidationを設ける

--- app/models/order_shipping_address_form.rb
+++ app/models/order_shipping_address_form.rb
@@ -9,7 +9,7 @@ class OrderShippingAddressForm
   }
 
   # 空白での投稿を禁止
-  validates :postal_code, :city, :house_num, :phone_number, presence: true
+  validates :postal_code, :city, :house_num, :phone_number, :token, presence: true
 
   # 空白である選択を禁止
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
