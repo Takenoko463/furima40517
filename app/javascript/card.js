4f4926f36e9f534596568d0fcebd3b328cd21380
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-04-10 15:42:47 +0900

    ボタンを押して実行されるかの確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,9 @@
 const pay = () => {
-    console.log("カード情報トークン化のためのJavaScript");
+    const form = document.getElementById('charge-form')
+    form.addEventListener("submit", (e) => {
+        console.log("フォーム送信時にイベント発火")
+        e.preventDefault();
+    });
 };
 
 window.addEventListener("turbo:load", pay);
